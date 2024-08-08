88568a9609ee3bdde57a12b7d2d4f34a9f4d279e
Eric <ericchiuboy@gmail.com>
2024-08-08 14:54:00 +0800

    new

--- plot_daloss.sh
+++
@@ -1,4 +0,0 @@
-log=$1
-
-python3 tools/analysis_tools/analyze_logs.py plot_curve outputs/$1 --keys da_loss0
-
--- plot_mAP_50.sh
+++
@@ -1,4 +0,0 @@
-
-
-python3 tools/analysis_tools/analyze_logs.py plot_curve outputs/$1 --keys bbox_mAP
-
